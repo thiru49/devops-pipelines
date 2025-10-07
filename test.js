48d0220532c2b7c8f8f7599ceb5e9ad44a38cad6
Thiruppathi <thiruppathi497@gmail.com>
2025-10-07 12:18:46 +0530

    feat: add ci pipeline

---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Hello devops pipelines start");
+setTimeout(()=>{
+    console.log("Hello devops this ci/cd pipelines in git actions");
+}, 3000);
+console.log("Hello devops pipelines end");
